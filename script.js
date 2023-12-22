fefe50a29c3c392eb50aecae9aeec8db61e39885
Mateus Brito <135028665+MateusBrito1@users.noreply.github.com>
2023-12-21 21:02:53 -0300

    att
    code improvements

--- script.js
+++ script.js
@@ -1,28 +1,31 @@
-let naoButton = document.getElementById('nao-button');
-naoButton.addEventListener('mouseover', moveNaoButton);
+const noButton = document.getElementById('no-button');
+const yesButton = document.getElementById('yes-button');
 
-function moveNaoButton() {
-    let maxWidth = window.innerWidth - naoButton.clientWidth;
-    let maxHeight = window.innerHeight - naoButton.clientHeight;
-    let randomX = Math.floor(Math.random() * maxWidth);
-    let randomY = Math.floor(Math.random() * maxHeight);
-    naoButton.style.position = 'absolute';
-    naoButton.style.left = randomX + 'px';
-    naoButton.style.top = randomY + 'px';
+noButton.addEventListener('mouseover', moveNoButton);
+noButton.addEventListener('mouseenter', moveNoButton)
+
+yesButton.addEventListener('click', () => {
+    const container = document.getElementById('container');
+
+    container.innerHTML = '';
+
+    const message = document.createElement('h2');
+    message.textContent = 'Quero viver pra sempre com você, você é o meu maior bem, te amo vitorinha!';
+    container.appendChild(message);
+
+    const gif = document.createElement('img');
+    gif.src = 'https://i.pinimg.com/originals/b5/e2/45/b5e245cfd05ae7f8fd026f44994ff42b.gif';
+    gif.style.width = '150px';
+    container.appendChild(gif); 
+});
+
+function moveNoButton() {
+    const maxWidth = window.innerWidth - noButton.clientWidth;
+    const maxHeight = window.innerHeight - noButton.clientHeight;
+    const randomX = Math.floor(Math.random () * maxWidth);
+    const randomY = Math.floor(Math.random () * maxHeight);
+
+    noButton.style.position = 'aboslute';
+    noButton.style.left = randomX + 'px';
+    noButton.style.top = randomY + 'px';
 }
-
-naoButton.addEventListener('mouseenter', function() {
-    moveNaoButton();
-});
-
-let simButton = document.getElementById('sim-button');
-simButton.addEventListener('click', function() {
-    let container = document.getElementById('container');
-    container.innerHTML = '<h2>Quero viver pra sempre com você, você é o meu maior bem, te amo muito vitorinha!</h2>';
-
-
-    let myGif = document.createElement("img");
-    myGif.src = "https://i.pinimg.com/originals/88/27/4b/88274b5f2aef162185bed9f359ece0cd.gif";
-    myGif.style.width = '150px';
-    container.appendChild(myGif);
-});
