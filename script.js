768e6720036ff756faad10397621d8a3f73be003
Mateus Brito <135028665+MateusBrito1@users.noreply.github.com>
2023-12-21 21:18:14 -0300

    attt
    refatore coding, delete gif.

--- script.js
+++ script.js
@@ -2,7 +2,6 @@ const noButton = document.getElementById('no-button');
 const yesButton = document.getElementById('yes-button');
 
 noButton.addEventListener('mouseover', moveNoButton);
-noButton.addEventListener('mouseenter', moveNoButton)
 
 yesButton.addEventListener('click', () => {
     const container = document.getElementById('container');
@@ -10,22 +9,22 @@ yesButton.addEventListener('click', () => {
     container.innerHTML = '';
 
     const message = document.createElement('h2');
-    message.textContent = 'Quero viver pra sempre com você, você é o meu maior bem, te amo vitorinha!';
+    message.textContent = 'Quero viver pra sempre com você, você é o meu maior bem, te amo Vitorinha!';
     container.appendChild(message);
 
     const gif = document.createElement('img');
     gif.src = 'https://i.pinimg.com/originals/b5/e2/45/b5e245cfd05ae7f8fd026f44994ff42b.gif';
     gif.style.width = '150px';
-    container.appendChild(gif); 
+    container.appendChild(gif);
 });
 
 function moveNoButton() {
     const maxWidth = window.innerWidth - noButton.clientWidth;
     const maxHeight = window.innerHeight - noButton.clientHeight;
-    const randomX = Math.floor(Math.random () * maxWidth);
-    const randomY = Math.floor(Math.random () * maxHeight);
+    const randomX = Math.floor(Math.random() * maxWidth);
+    const randomY = Math.floor(Math.random() * maxHeight);
 
-    noButton.style.position = 'aboslute';
+    noButton.style.position = 'absolute';
     noButton.style.left = randomX + 'px';
     noButton.style.top = randomY + 'px';
 }
